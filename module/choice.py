e67cb7507526fb4e38dc86627635c78db48d1785
RLettuce <syunrobin@gmail.com>
2022-06-24 16:16:59 +0900

    ✒choice.py: complete

--- module/choice.py
+++ module/choice.py
@@ -1,2 +1,18 @@
-def choice():
-    
+import json
+
+def choice(json_file_pass, key):
+    json_data = json.load(open(json_file_pass))
+    check = json_data[key]["check"]
+    if check == "True":
+        check = True
+    elif check == "False":
+        check = False
+    else:
+        print("An error occurred: json file is not correct")
+    json_data = json_data[key]["choices"]
+    number_of_choices = json_data.values()
+    for i in range(number_of_choices):
+        outline = str(i) + json_data[i]
+        print(outline)
+    return [number_of_choices, check]
+
