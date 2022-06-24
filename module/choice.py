32b3f636b293a7fcece848d2c7a75630fb8783f2
RLettuce <syunrobin@gmail.com>
2022-06-24 16:45:54 +0900

    ✒choice: debug; char code, check unsuted

--- module/choice.py
+++ module/choice.py
@@ -1,18 +1,17 @@
 import json
 
 def choice(json_file_pass, key):
-    json_data = json.load(open(json_file_pass))
+    json_data = json.load(open(json_file_pass, "r", encoding="utf-8_sig"))
     check = json_data[key]["check"]
     if check == "True":
         check = True
     elif check == "False":
         check = False
     else:
-        print("An error occurred: json file is not correct")
+        print("An error occurred: json file is not correct!!")
     json_data = json_data[key]["choices"]
-    number_of_choices = json_data.values()
+    number_of_choices = len(json_data)
     for i in range(number_of_choices):
-        outline = str(i) + json_data[i]
+        outline = str(i) + ": " + json_data[i]
         print(outline)
     return [number_of_choices, check]
-
