8921ee0547a6e9254501dffdbed23c77230126af
syunsuke okamura <syunrobin@gmail.com>
2022-06-29 22:04:08 +0900

    ✒status.py: status class constructor def

--- module/status.py
+++ module/status.py
@@ -10,5 +10,10 @@ def Load(json_file_pass):
     json_file = json.load(json_file)
     return json_file
 
-class status_manager:
-    
+class status:
+
+    def __init__(self, *status):
+        self.status_data = {"status": {}}
+        if status:
+            for i in range(len(status)):
+                self.status_data["status"].update((status[0],status[1]))
